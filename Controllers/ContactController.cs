1e3ffe33023bc5b04f4eaa3774b39e53d5f93ec8
Dilan Lopez <dilanlopez009@gmail.com>
2023-02-27 17:10:14 -0300

    contact list

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -2,15 +2,27 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ASPNetMVC.Context;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ASPNetMVC.Controllers
 {
   public class ContactController : Controller
   {
+
+    private readonly ContackBookContext _context;
+
+    public ContactController(ContackBookContext context)
+    {
+      _context = context;
+    }
+
+
     public IActionResult Index()
     {
-      return View();
+      var contacts = _context.Contacts.ToList();
+      return View(contacts);
     }
+
   }
 }
